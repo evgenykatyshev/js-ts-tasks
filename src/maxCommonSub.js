cb5a6e9ae9c039de118f3df0a28acbaadf967c47
Evgeny Katyshev <katyshev.evgenii@student.ehu.lt>
2024-11-21 11:00:07 +0400

    feat: solving task maxCommonSub

--- src/maxCommonSub.js
+++ src/maxCommonSub.js
@@ -6,8 +6,43 @@
  * answer: 'gabcdef' - a common substring for both strings
  * @param {string} str1
  * @param {string} str2
- * @returns {number}
+ * @returns {string}
  */
 module.exports.maxCommonSub = function maxCommonSub(str1, str2) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  let maxCommonSub = '';
+  let maxCommonSubLength = 0;
+  let maxCommonSubCandidate = '';
+  let maxCommonSubCandidateLength = 0;
+  let commonSubStr1Index;
+  let commonSubStr2Index;
+
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = 0; j < str2.length; j++) {
+      if ( str1[i] === str2[j] ) {
+        commonSubStr1Index = i;
+        commonSubStr2Index = j;
+
+        while ( str1[commonSubStr1Index] && str2[commonSubStr2Index] && ( str1[commonSubStr1Index] === str2[commonSubStr2Index] ) ) {
+          maxCommonSubCandidate = `${maxCommonSubCandidate}${str1[commonSubStr1Index]}`;
+
+          maxCommonSubCandidateLength++;
+
+          commonSubStr1Index++;
+          commonSubStr2Index++;
+        }
+
+        if (maxCommonSubCandidateLength > maxCommonSubLength) {
+          maxCommonSub = maxCommonSubCandidate;
+          maxCommonSubLength = maxCommonSubCandidateLength;
+        }
+
+        maxCommonSubCandidate = '';
+        maxCommonSubCandidateLength = 0;
+
+        j = commonSubStr2Index - 1;
+      }
+    }
+  }
+
+  return maxCommonSub;
 };
