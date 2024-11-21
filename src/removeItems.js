5829421670589de8ec0751155ef59a398d98d38c
Evgeny Katyshev <katyshev.evgenii@student.ehu.lt>
2024-11-21 11:18:48 +0400

    feat: solving task removeItems

--- src/removeItems.js
+++ src/removeItems.js
@@ -5,5 +5,13 @@
  * @returns {Object}
  */
 module.exports.removeItems = function removeItems(arr, value) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if ( JSON.stringify(arr[i]) !== JSON.stringify(value) ) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
 };
