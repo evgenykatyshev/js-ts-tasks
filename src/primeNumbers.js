acdab65e31760f94572e81d79b2be90f0eeb8848
Evgeny Katyshev <katyshev.evgenii@student.ehu.lt>
2024-11-22 23:59:01 +0400

    feat: solving task primeNumbers

--- src/primeNumbers.js
+++ src/primeNumbers.js
@@ -4,5 +4,28 @@
  * @returns {function}
  */
 module.exports.primeNumbers = function primeNumbers(highestNumber) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  return function(lowLimit, highLimit) {
+
+    let allNumbersWithinLimits = [];
+
+    let k = 2;
+
+    for (let i = lowLimit; i <= highLimit; i++) {
+      allNumbersWithinLimits.push(i);
+    }
+
+    for (let p = 2; p < highLimit; p++) {
+      k = 2;
+      while ( p * k <= highLimit ) {
+        for (let j = 0; j < allNumbersWithinLimits.length; j++) {
+          if (allNumbersWithinLimits[j] % ( p * k ) === 0 ) {
+            allNumbersWithinLimits.splice(j, 1);
+          }
+        }
+        k++;
+      }
+    }
+
+    return allNumbersWithinLimits;
+  }
 };
