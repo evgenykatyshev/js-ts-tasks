23ca83f933cbf45823fe56bdbe70fd3f5ee6ba41
evgenykatyshev <eugkat@gmail.com>
2024-12-11 19:46:40 +0400

    feat: preparing

--- src/all.js
+++ src/all.js
@@ -7,7 +7,7 @@ module.exports.all = function all(promisesArray) {
   // The Promise.all() method returns a single Promise from a list of promises, when all promises fulfill.
 
   for (let elem of promisesArray) {
-
+    test;
   }
 
   return singlePromise;
