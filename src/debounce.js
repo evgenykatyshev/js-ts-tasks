b7ba816203635046bff6ec5b88ae83ffe7a8dabc
evgenykatyshev <eugkat@gmail.com>
2024-12-15 19:57:25 +0400

    feat: solving 'debounce' task

--- src/debounce.js
+++ src/debounce.js
@@ -6,5 +6,15 @@
  * @return {function}
  */
 module.exports.debounce = function debounce(fn, delay) {
-  throw new Error('Not implemented'); // remove me and write your code
+  let isRunning = false;
+
+  return function (arg) {
+    if (!isRunning) {
+      isRunning = true;
+      fn(arg);
+      setTimeout(() => {
+        isRunning = false;
+      }, delay);
+    }
+  };
 };
